6ad20924bec4f1f45ee1d7b8cc49ecb74a4defb9
Shota IDE <ide.shota.dev@gmail.com>
2018-09-23 00:24:38 +0900

    #5 [del] 不要なパッケージ読み込みを削除

--- note/src/containers/Home.js
+++ note/src/containers/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 
 import request from 'superagent'
